ef73b7649f1aaebf0fb6bbf9a05d8152911cf021
Charles Chan <charles@Charless-MacBook-Pro.local>
2026-02-22 23:14:19 +1100

    chore: update main.ts with rawBody for Stripe webhook

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  await app.listen(process.env.PORT || 3000);
-}
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true, // Stripe webhook 需要
+  });
 
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 3000);
+}
 bootstrap();
