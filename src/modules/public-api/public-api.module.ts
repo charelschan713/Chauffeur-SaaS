e26e6057afe4b90113bbc60065a09f62442df1ef
Charles Chan <charles@charless-mbp.lan>
2026-02-24 16:03:56 +1100

    feat: public bookings API - setup intent + charge

--- src/modules/public-api/public-api.module.ts
+++ src/modules/public-api/public-api.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { PublicApiController, PublicOpenController } from './public-api.controller';
 import { PublicApiService } from './public-api.service';
 import { QuoteCalculatorService } from './quote-calculator.service';
+import { PublicBookingsController } from './public-bookings.controller';
+import { PublicBookingsService } from './public-bookings.service';
 import { TenantsModule } from '../tenants/tenants.module';
 
 @Module({
   imports: [TenantsModule],
-  controllers: [PublicOpenController, PublicApiController],
-  providers: [PublicApiService, QuoteCalculatorService],
+  controllers: [PublicOpenController, PublicApiController, PublicBookingsController],
+  providers: [PublicApiService, QuoteCalculatorService, PublicBookingsService],
 })
 export class PublicApiModule {}
