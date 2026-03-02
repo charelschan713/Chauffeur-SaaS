242a701c6fd7841345a901dff09f38f37402fdeb
Charles Chan <charleschan@charless-mbp.lan>
2026-03-02 12:16:11 +1100

    fix: import NotificationModule in BookingModule

--- src/booking/booking.module.ts
+++ src/booking/booking.module.ts
@@ -3,9 +3,10 @@ import { BookingService } from './booking.service';
 import { BookingController } from './booking.controller';
 import { OutboxWorkerService } from './outbox-worker.service';
 import { PricingModule } from '../pricing/pricing.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
-  imports: [PricingModule],
+  imports: [PricingModule, NotificationModule],
   providers: [BookingService, OutboxWorkerService],
   controllers: [BookingController],
   exports: [BookingService],
